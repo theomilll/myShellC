66a7677bcdcbc1e0e94de14e236a6d76f5cb3500
theomilll <tam4@cesar.school>
2023-09-21 21:00:15 -0300

    .

--- shell.c
+++ shell.c
@@ -4,6 +4,7 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <pthread.h>
 
 #define MAX_INPUT_SIZE 1024
 #define MAX_ARG_SIZE 100
@@ -32,15 +33,13 @@ void executeCommand(char **parsedArgs, enum ExecStyle style) {
     int pipefd[2];
     pid_t pid1, pid2;
 
-    // Check for pipe symbol
     int hasPipe = 0;
     char *parsedArgs2[MAX_ARG_SIZE];
     for (int i = 0; parsedArgs[i] != NULL; i++) {
         if (strcmp(parsedArgs[i], "|") == 0) {
             hasPipe = 1;
-            parsedArgs[i] = NULL;  // Split the commands at the pipe symbol
+            parsedArgs[i] = NULL;
 
-            // Prepare for the second command
             int j = 0;
             i++;
             for (; parsedArgs[i] != NULL; i++, j++) {
@@ -54,17 +53,13 @@ void executeCommand(char **parsedArgs, enum ExecStyle style) {
     char *inputFile = NULL;
     char *outputFile = NULL;
 
-    // If there is a pipe
     if (hasPipe) {
-        // Create pipe
         if (pipe(pipefd) < 0) {
             perror("pipe");
             return;
         }
 
-        // Fork first process
         if ((pid1 = fork()) == 0) {
-            // Redirect stdout to pipe
             dup2(pipefd[1], STDOUT_FILENO);
             close(pipefd[0]);
             execvp(parsedArgs[0], parsedArgs);
@@ -72,9 +67,7 @@ void executeCommand(char **parsedArgs, enum ExecStyle style) {
             exit(1);
         }
 
-        // Fork second process
         if ((pid2 = fork()) == 0) {
-            // Redirect stdin from pipe
             dup2(pipefd[0], STDIN_FILENO);
             close(pipefd[1]);
             execvp(parsedArgs2[0], parsedArgs2);
@@ -82,14 +75,12 @@ void executeCommand(char **parsedArgs, enum ExecStyle style) {
             exit(1);
         }
 
-        // Parent closes unused file descriptors and waits for children
         close(pipefd[0]);
         close(pipefd[1]);
         wait(NULL);
         wait(NULL);
 
     } else {
-        // Check for input and output redirection here
         for (int i = 0; parsedArgs[i] != NULL; i++) {
             if (strcmp(parsedArgs[i], "<") == 0) {
                 parsedArgs[i] = NULL;
@@ -108,14 +99,10 @@ void executeCommand(char **parsedArgs, enum ExecStyle style) {
 
         pid = fork();
         if (pid == 0) {
-            // Child process
-
-            // Handle input redirection if specified
             if (inputFile) {
                 freopen(inputFile, "r", stdin);
             }
 
-            // Handle output redirection if specified
             if (outputFile) {
                 freopen(outputFile, "w", stdout);
             }
@@ -125,102 +112,107 @@ void executeCommand(char **parsedArgs, enum ExecStyle style) {
             }
             exit(0);
         } else {
-            // Parent process
             if (style == SEQUENTIAL) {
-                waitpid(pid, &status, 0);  // Wait for child to exit
+                waitpid(pid, &status, 0);
             }
         }
-        // printf("Debug: Command to execute: %s\n", parsedArgs[0]);
-
-    // if (pid == 0) {
-    //     // Child process
-
-    //     // Debug print to see if inputFile and outputFile are correctly set
-    //     if (inputFile) {
-    //         printf("Debug: Input file: %s\n", inputFile);
-    //     }
-    //     if (outputFile) {
-    //         printf("Debug: Output file: %s\n", outputFile);
-    //     }
-
-    //     if (execvp(parsedArgs[0], parsedArgs) < 0) {
-    //         printf("Could not execute command: %s\n", parsedArgs[0]);
-    //     }
-    //     exit(0);
-    // }
     }
 }
 
-int main() {
-    // Define the history array and counter
+void *threadExecuteCommand(void *arg) {
+    char **parsedArgs = (char **) arg;
+    executeCommand(parsedArgs, PARALLEL);
+    pthread_exit(0);
+}
+
+int main(int argc, char *argv[]) {
     #define MAX_HISTORY 100
     char *commandHistory[MAX_HISTORY];
     int historyCount = 0;
-    
+    pthread_t threads[MAX_COMMANDS];
+
     char input[MAX_INPUT_SIZE];
     char *parsedArgs[MAX_ARG_SIZE];
     char *commands[MAX_COMMANDS];
-    enum ExecStyle style = SEQUENTIAL;  // Default style is sequential
-    pid_t wpid;  // Declare wpid here
+    enum ExecStyle style = SEQUENTIAL;
+    pid_t wpid;
 
-    while (1) {
-        // Print the shell prompt
-        printf("\nmyShell> ");
-        // Take input from user
-        if (fgets(input, MAX_INPUT_SIZE, stdin) == NULL) {
-            perror("Failed to read input");
+    FILE *batchFile = NULL;
+    
+    if (argc == 2) {
+        batchFile = fopen(argv[1], "r");
+        if (batchFile == NULL) {
+            perror("Error opening batch file");
             exit(EXIT_FAILURE);
         }
-        
-        // Remove trailing newline character from input
+    }
+    
+
+    while (1) {
+        if (batchFile) {
+            if (fgets(input, MAX_INPUT_SIZE, batchFile) == NULL) {
+                fclose(batchFile);
+                break;
+            }
+        } else {
+            printf("\nShelldon> ");
+            if (fgets(input, MAX_INPUT_SIZE, stdin) == NULL) {
+                perror("Failed to read input");
+                exit(EXIT_FAILURE);
+            }
+        }
         input[strcspn(input, "\n")] = 0;
         
-        // Add this block here
-        // Add the command to history only if it's not "!!"
         if (strcmp(input, "!!") != 0) {
             if (historyCount < MAX_HISTORY) {
                 commandHistory[historyCount++] = strdup(input);
-            } else {
-                // Implement logic to remove the oldest command and add the newest
-                // if the history is full.
             }
         }
-        // End of new block
-        
-        // Split input into multiple commands separated by ;
         splitCommands(input, commands);
+        for (int i = 0; commands[i] != NULL; i++) {
+            parseInput(commands[i], parsedArgs);
+
+            if (style == SEQUENTIAL) {
+                executeCommand(parsedArgs, style);
+            } else { // Modo PARALLEL
+                pthread_create(&threads[i], NULL, threadExecuteCommand, (void *)parsedArgs);
+            }
+        }
 
         for (int i = 0; commands[i] != NULL; i++) {
-            // Parse each command to separate arguments and options
             parseInput(commands[i], parsedArgs);
             
-            // Handle !! command
             if (strcmp(parsedArgs[0], "!!") == 0) {
                 if (historyCount > 0) {
-                    // Execute the last command from history
                     char *lastCommand = commandHistory[historyCount - 1];
-                    printf("Executing last command: %s\n", lastCommand);  // Debug print
+                    printf("Executing last command: %s\n", lastCommand);
 
-                    // Parse and execute this lastCommand
                     parseInput(lastCommand, parsedArgs);
                     executeCommand(parsedArgs, style);
                 } else {
                     printf("No commands in history.\n");
                 }
-                continue;  // Skip to next iteration of loop
+                continue;
             }
-            // Check for style change commands
             if (strcmp(parsedArgs[0], "style") == 0 && parsedArgs[1] != NULL) {
-                // /* Your existing code here */
             }
+            if (strcmp(parsedArgs[0], "style") == 0 && parsedArgs[1] != NULL) {
+                if (strcmp(parsedArgs[1], "sequential") == 0) {
+                    style = SEQUENTIAL;
+                } else if (strcmp(parsedArgs[1], "parallel") == 0) {
+                    style = PARALLEL;
+                }
+                continue;
+            }
+
             
-            // Execute each command based on the style (Sequential/Parallel)
             executeCommand(parsedArgs, style);
         }
         
-        // If it's parallel, wait for all child processes to complete
         if (style == PARALLEL) {
-            while ((wpid = wait(NULL)) > 0);
+            for (int i = 0; commands[i] != NULL; i++) {
+                pthread_join(threads[i], NULL);
+            }
         }
     }
     return 0;
